03571f5517ec9b3140afd77c035db45d4ae91c10
Rocio Penny <rbarrag2@asu.edu>
2026-03-18 02:37:53 -0400

    Corrected commit message

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -110,3 +110,36 @@ board[i][i] = '-';
 
 }
 
+
+ 
+
+    public void displayBoard() {
+
+        System.out.println("Current Board:");
+
+        System.out.println("  1 2 3");
+
+        for (int i = 0; i < 3; i++) {
+
+            System.out.print((i + 1) + " ");
+
+            for (int j = 0; j < 3; j++) {
+
+                System.out.print(board[i][j] + " ");
+
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+ 
+
+    public char getCurrentPlayer() {
+
+        return currentPlayer;
+
+    }
+
